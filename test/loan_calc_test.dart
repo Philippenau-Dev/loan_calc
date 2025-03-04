73169a375209f62456406358cc775f25efd421bb
Philippe Nau Rosa <philippe.rosa@premiersoft.net>
2025-03-04 11:14:27 -0300

    fix tests

--- test/loan_calc_test.dart
+++ test/loan_calc_test.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:loan_calc/loan_cal.dart';
+import 'package:loan_calc/loan_calc.dart';
 import 'package:xirr_flutter/xirr_flutter.dart';
 
 void main() {
